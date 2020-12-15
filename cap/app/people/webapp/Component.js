95caeae28a2fa37c35325d6db0615b6c7a11a627
Thomas Jung <thomas.jung@sap.com>
2020-12-15 18:20:56 -0500

    Add Generated Fiori for People

---
+++ cap/app/people/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("star.wars.people.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
