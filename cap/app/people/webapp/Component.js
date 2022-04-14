a274fc82d54576ca8d1d1cb39c79d24acc22fe24
Thomas Jung <thomas.jung@sap.com>
2022-04-14 15:56:16 -0400

    Update UI5 Bootstrapping

--- cap/app/people/webapp/Component.js
+++ cap/app/people/webapp/Component.js
@@ -1,9 +1,10 @@
-sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
+sap.ui.define(['sap/fe/core/AppComponent'], (AppComponent) => {
     'use strict';
 
     return AppComponent.extend("star.wars.people.Component", {
         metadata: {
             manifest: "json"
         }
-    });
-});
+        
+    })
+})
