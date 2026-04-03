d189308ca3f0080d38a21bf4e13a0eba88d095bf
Thomas Jung <thomas.jung@sap.com>
2026-04-03 19:09:11 -0400

    feat: add Star Wars Films Fiori webapp

---
+++ cap/app/film/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/fe/core/AppComponent'], (AppComponent) => {
+    'use strict';
+
+    return AppComponent.extend("star.wars.film.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    })
+})
