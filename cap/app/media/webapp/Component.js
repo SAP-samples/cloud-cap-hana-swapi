4d0a1a3db96402e751ac08b815f63faedf189cfc
Thomas Jung <thomas.jung@sap.com>
2026-04-03 18:30:04 -0400

    feat: add Star Wars Media Browser Fiori webapp

---
+++ cap/app/media/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/fe/core/AppComponent'], (AppComponent) => {
+    'use strict';
+
+    return AppComponent.extend("star.wars.media.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    })
+})
