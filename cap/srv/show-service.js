849cb59e5db89b4fdf06676e8c0b549822ff9dbf
Thomas Jung <thomas.jung@sap.com>
2026-04-03 21:52:02 -0400

    feat: update Media edit_url to Film/Show webapps and add Film/Show tiles to launchpad

--- cap/srv/show-service.js
+++ cap/srv/show-service.js
@@ -5,13 +5,12 @@ const cds = require('@sap/cds')
 module.exports = cds.service.impl(function () {
 
     // Populate the virtual edit_url field after every Media READ.
-    // Directs the user to the Fiori Preview for the appropriate service.
-    // Note: /$fiori-preview/ is a CAP dev tool endpoint — not available in production.
+    // Directs the user to the real Film/Show webapps using deep-link URLs.
     this.after('READ', 'Media', results => {
         for (const m of [].concat(results)) {
             m.edit_url = m.media_type === 'FILM'
-                ? '/$fiori-preview/StarWarsFilm/Film#preview-app'
-                : '/$fiori-preview/StarWarsShow/Show#preview-app'
+                ? `/film/webapp/index.html#/Film(${m.ID})`
+                : `/show/webapp/index.html#/Show(${m.ID})`
         }
     })
 
