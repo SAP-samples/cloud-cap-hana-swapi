20e0ccc1d3ff8ffd5c770d0178dc1a5b10d21c70
Thomas Jung <thomas.jung@sap.com>
2026-05-01 14:58:39 -0500

    feat: add ChangeView entity and update changelog structure

--- cap/@cds-models/sap/changelog/index.js
+++ cap/@cds-models/sap/changelog/index.js
@@ -6,9 +6,12 @@ module.exports.aspect_ = createEntityProxy(['sap.changelog', 'aspect'], { target
 // ChangeView
 module.exports.ChangeView = createEntityProxy(['sap.changelog', 'ChangeView'], { target: { is_singular: true }, customProps: ["modification"] })
 module.exports.ChangeView_ = createEntityProxy(['sap.changelog', 'ChangeView'], { target: { is_singular: false }})
-// ChangeLog
-module.exports.ChangeLog = createEntityProxy(['sap.changelog', 'ChangeLog'], { target: { is_singular: true } })
-module.exports.ChangeLog_ = createEntityProxy(['sap.changelog', 'ChangeLog'], { target: { is_singular: false }})
+// i18nKeys
+module.exports.i18nKey = createEntityProxy(['sap.changelog', 'i18nKeys'], { target: { is_singular: true } })
+module.exports.i18nKeys = createEntityProxy(['sap.changelog', 'i18nKeys'], { target: { is_singular: false }})
+// CHANGE_TRACKING_DUMMY
+module.exports.CHANGE_TRACKING_DUMMY = createEntityProxy(['sap.changelog', 'CHANGE_TRACKING_DUMMY'], { target: { is_singular: true } })
+module.exports.CHANGE_TRACKING_DUMMY_ = createEntityProxy(['sap.changelog', 'CHANGE_TRACKING_DUMMY'], { target: { is_singular: false }})
 // Changes
 module.exports.Change = createEntityProxy(['sap.changelog', 'Changes'], { target: { is_singular: true }, customProps: ["modification"] })
 module.exports.Changes = createEntityProxy(['sap.changelog', 'Changes'], { target: { is_singular: false }})
